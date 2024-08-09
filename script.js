21a2dad7aafadbf78bd3d375d7be28d4688ab1e4
sudarshan9933 <sudarshantripathi00@gmail.com>
2024-08-09 20:14:01 +0530

    done

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ async function fetchData(query) {
         }
         
         const data = await response.json();
+        console.log('data is resolved', data);
         return data;
     } catch (error) {
         console.error("Error fetching data:", error.message);
